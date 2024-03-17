ca495a94e25a14c6930dbefd3dc0d5518ddd16c8
Ank1tGupta <ankitgup82@gmail.com>
2024-03-17 18:09:30 +0530

    Adding Dashboard

---
+++ script.js
@@ -0,0 +1,12 @@
+function myfile(){
+    window.location.href='myfiles.html'
+}
+function templates(){
+    window.location.href='templates.html'
+}
+function users(){
+    window.location.href='users.html'
+}
+function login(){
+    window.location.href='Login.html'
+}
